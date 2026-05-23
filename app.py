3bb89a018f4fd66a8852db322692c9be0fc38a09
sarahsitishalsabila8-debug <sarahsitishalsabila8@gmail.com>
2026-05-23 10:11:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
-
+t.set_page_config(
+page_title="FinanceDashboard",
+layout="wide"
+)
+#Hirarkiteks
 st.title("📊Dashboard")
 st.header("LaporanBulanan")
 st.subheader("📈MonthlyExpenses")
